73a02d6f6b53b09bef4b1b28499b2b08e56f266a
edy.rifeni <edy.rifeni@students.amikom.ac.id>
2019-01-08 11:17:05 +0700

    adding some Function

---
+++ app/src/main/java/app/tugas/finalproject/feature/base/BaseActivity.kt
@@ -0,0 +1,43 @@
+package app.tugas.finalproject.feature.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import app.tugas.finalproject.helper.invisible
+import app.tugas.finalproject.helper.visible
+import dagger.android.AndroidInjection
+import org.jetbrains.anko.contentView
+import org.jetbrains.anko.design.snackbar
+
+abstract class BaseActivity<out T : BasePresenter> : AppCompatActivity(), BaseView {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
+        onInitView()
+
+        getPresenter()?.run { this.onAttach() }
+    }
+
+    protected abstract fun onInitView()
+
+    protected abstract fun getPresenter() : T?
+
+    override fun showMessage(message: String) {
+        this.contentView?.rootView?.let {
+            snackbar(it, message)
+        }
+    }
+
+    override fun onDestroy() {
+        getPresenter()?.onDetach()
+        super.onDestroy()
+    }
+
+    override fun showLoading() {
+        getProgressBar()?.visible()
+    }
+
+    override fun hideLoading() {
+        getProgressBar()?.invisible()
+    }
+}
